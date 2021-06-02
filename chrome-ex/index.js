7a76526735eddbfe4654c088aacfe401338e18d7
Mike Robards <mikerobards@icloud.com>
2021-06-02 15:49:31 -0400

    use string for html li

--- chrome-ex/index.js
+++ chrome-ex/index.js
@@ -2,6 +2,7 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
+let listItems = ""
 
 inputBtn.addEventListener('click', saveLead)
 
@@ -10,14 +11,18 @@ function saveLead() {
     console.log(myLeads)
 }
 
-for (let i=0;i<myLeads.length;i++) {
-    console.log(myLeads[i])
-}
+// for (let i=0;i<myLeads.length;i++) {
+//     console.log(myLeads[i])
+// }
 
 myLeads.forEach(i => {
-    const li = document.createElement('li')
-    li.textContent = i
-    ulEl.append(li)
+    listItems += `<li>${i}</li>` 
+    
+    // const li = document.createElement('li')
+    // li.textContent = i
+    // ulEl.append(li)
     // ulEl.appendChild(`<li>${i}</li>`)
 })
+console.log(listItems)
+ulEl.innerHTML = listItems
 
