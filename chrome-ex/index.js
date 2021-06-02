65ba3af9186b95c0b1a3358bd2dd3212c97c32a2
Mike Robards <mikerobards@icloud.com>
2021-06-02 16:03:18 -0400

    refactor

--- chrome-ex/index.js
+++ chrome-ex/index.js
@@ -1,28 +1,23 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 let listItems = ""
 
-inputBtn.addEventListener('click', saveLead)
-
-function saveLead() {
+inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    myLeads.forEach(i => {
+        listItems += `<li>${i}</li>` 
+    })
+    console.log(listItems)
+    ulEl.innerHTML = listItems
 }
 
-// for (let i=0;i<myLeads.length;i++) {
-//     console.log(myLeads[i])
-// }
 
-myLeads.forEach(i => {
-    listItems += `<li>${i}</li>` 
-    
-    // const li = document.createElement('li')
-    // li.textContent = i
-    // ulEl.append(li)
-    // ulEl.appendChild(`<li>${i}</li>`)
-})
-console.log(listItems)
-ulEl.innerHTML = listItems
+
 
