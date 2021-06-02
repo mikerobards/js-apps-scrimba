a043cfbd1970ccefb3c5b63f66cbbd0ab591d221
Mike Robards <mikerobards@icloud.com>
2021-06-02 15:29:53 -0400

    add li

--- chrome-ex/index.js
+++ chrome-ex/index.js
@@ -1,7 +1,18 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener('click', saveLead)
 
 function saveLead() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 }
+
+for (let i=0;i<myLeads.length;i++) {
+    console.log(myLeads[i])
+}
+
+myLeads.forEach(i => ulEl.innerHTML += `<li>${i}</li>`)
+
